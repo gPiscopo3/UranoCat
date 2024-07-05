7046a2818317e29df7f51fac3f8b32c8330675a8
g.piscopo3 <piscopogiuseppe1@hotmail.it>
2024-07-05 12:31:47 +0200

    Added Simple Movement

---
+++ Assets/Controls/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] protected CharacterController characterController;
+    protected CharacterStatus status;
+
+    private void Awake()
+    {
+       characterController = GetComponent<CharacterController>(); 
+       status = GetComponent<CharacterStatus>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0, 0, 0);
+        if (status.IsMoving)
+        {
+            movement.z = status.Movement.z*Time.deltaTime;
+        }
+        movement = transform.TransformDirection(movement);
+        characterController.SimpleMove(movement);
+        if (status.IsRotating)
+        {
+            transform.Rotate(0, status.Rotation.y, 0);
+        }
+    }
+
+}
