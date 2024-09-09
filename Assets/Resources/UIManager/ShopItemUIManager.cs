d2c0bfb0ab1b9d62e914cf3135905bfc73c726aa
alicesalierno <alice.salierno@gmail.com>
2024-09-09 19:14:30 +0200

    Shop funzionante con filtraggio

--- Assets/Resources/UIManager/ShopItemUIManager.cs
+++ Assets/Resources/UIManager/ShopItemUIManager.cs
@@ -13,7 +13,7 @@ public class ShopItemUIManager : MonoBehaviour
     [SerializeField] float itemSpacing = 35.5f;
     float itemWidth;
     float itemHeight;
-
+    float diff;
 
 
     [Header("UI Elements")]
@@ -39,53 +39,52 @@ public class ShopItemUIManager : MonoBehaviour
         this.shopItems = FindObjectOfType<ItemLoader>().shopItems;
         
         this.player = FindObjectOfType<PlayerLoader>().player;
-
+        CreateShopUI();
         
 
         keyItemButton.onClick.AddListener(FilteringKeyItem);
-        
-        GenerateShopItemUI(typeof(CatItem));
+        catItemButton.onClick.AddListener(FilteringCatItem);
+        FilteringCatItem();
 
 
     }
 
-    public void CreateShop(){
+    public void CreateShopUI(){
+        
+        // recupero la larghezza e l'altezza dello shop item
+        itemWidth = contentPanel.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.x;
+        itemHeight = contentPanel.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.y;
 
-
-
+        // recupero la differenza tra l'altezza del contentPanel e l'altezza dello shop item
+        diff = (contentPanel.GetComponent<RectTransform>().sizeDelta.y - contentPanel.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.y)/2;
+        Debug.Log("differenza " + diff);
+        
     }
 
 
 
     public void GenerateShopItemUI(Type type){
 
-        // recupero la larghezza e l'altezza dello shop item
-        itemWidth = contentPanel.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.x;
-        Debug.Log("alte " + itemWidth);
 
-        itemHeight = contentPanel.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.y;
-
-
-// recupero la differenza tra l'altezza del contentPanel e l'altezza dello shop item
-        float diff = (contentPanel.GetComponent<RectTransform>().sizeDelta.y - contentPanel.transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.y)/2;
-        Debug.Log("differenza " + diff);
-
-        for(int j=0; j<contentPanel.transform.childCount; j++){
-
-            Destroy(contentPanel.transform.GetChild(j).gameObject);
-        }
-            
-        //contentPanel.transform.DetachChildren();
-
-
+        for(int j=1; j<contentPanel.transform.childCount; j++){
         
+           Destroy(contentPanel.transform.GetChild(j).gameObject);
+           
+        }
+        contentPanel.transform.GetChild(0).gameObject.SetActive(false);
+        
+        //contentPanel.transform.DetachChildren();
+        
+
         int i = 0;
         
         foreach(ShopItem shopItem in shopItems)
         {
             if(shopItem.item.GetType().Equals(type)){
-
+                
                 ShopItemUI shopItemUI = Instantiate(shopItemPrefab, contentPanel.transform).GetComponent<ShopItemUI>();
+                shopItemUI.gameObject.SetActive(true);
+
                 shopItemUI.GetComponent<Image>().enabled = true;
 
 
@@ -100,11 +99,13 @@ public class ShopItemUIManager : MonoBehaviour
                 shopItemUI.SetShopItemImage(shopItem.item.imagePath);
 
                 
-                shopItemUI.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, diff, itemHeight);
+                
                 //shopItemUI.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, diff, itemHeight);
-
+                
+                
+                shopItemUI.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, diff, itemHeight);
                 contentPanel.GetComponent<RectTransform>().sizeDelta= Vector2.right * (itemWidth + itemSpacing) * shopItems.Count;
-
+                
                 i++; 
             }
             
@@ -116,8 +117,12 @@ public class ShopItemUIManager : MonoBehaviour
 
 
     public void FilteringKeyItem(){
-
         GenerateShopItemUI(typeof(KeyItem));
+
+    }
+    
+    public void FilteringCatItem(){
+        GenerateShopItemUI(typeof(CatItem));
 
     }
     
