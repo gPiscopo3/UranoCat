c37bc5580ddef50bb10b461af7faa95e9adb5fc2
alicesalierno <alice.salierno@gmail.com>
2024-09-10 10:16:24 +0200

    fix

--- Assets/Resources/Model/KeyItem.cs
+++ Assets/Resources/Model/KeyItem.cs
@@ -5,23 +5,16 @@ using UnityEngine;
 public class KeyItem : Item
 {
     
-    public KeyItemType keyItemType;
 
     public KeyItem() {}
 
-    public KeyItem(string tag, string name, string description, KeyItemType keyItemType, string imagePath)
+    public KeyItem(string tag, string name, string description, string imagePath)
     {
         this.tag = tag;
         this.name = name;
         this.descrizione = description;
-        this.keyItemType = keyItemType;
         this.imagePath = imagePath;
     }
 
 }
 
-public enum KeyItemType
-{
-    UTENSILE, 
-    CONSUMABILE
-}
