faa0ff8491e339bd2c71749594b72cb54dd5ee73
alicesalierno <alice.salierno@gmail.com>
2024-09-10 10:20:31 +0200

    fix

--- Assets/Resources/Model/KeyItem.cs
+++ Assets/Resources/Model/KeyItem.cs
@@ -8,12 +8,13 @@ public class KeyItem : Item
 
     public KeyItem() {}
 
-    public KeyItem(string tag, string name, string description, string imagePath)
+    public KeyItem(string tag, string name, string description, int durability, string imagePath)
     {
         this.tag = tag;
         this.name = name;
         this.descrizione = description;
         this.imagePath = imagePath;
+        this.durability = durability;
     }
 
 }
