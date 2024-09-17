48938f8f0e3b7c9cfa36e671445d2c9f4e0e0e80
g.piscopo3 <piscopogiuseppe1@hotmail.it>
2024-09-17 16:13:35 +0200

    Fix

--- Assets/Resources/MenuManager/MainMenu.cs
+++ Assets/Resources/MenuManager/MainMenu.cs
@@ -10,15 +10,15 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private String mainMenu;
     public void NewGame(){
 
-        SaveInfo.profile = "profile";
-        SaveInfo.save = null;
+        SaveLoader.loaded_profile = "profile";
+        SaveLoader.loaded_save = null;
         SceneManager.LoadScene(mainMenu);
 
     }
 
     public void Continue(){
-        SaveInfo.profile = "profile";
-        SaveInfo.save = "save";
+        SaveLoader.loaded_profile = "profile";
+        SaveLoader.loaded_save = "save";
         SceneManager.LoadScene(mainMenu);
 
     }
