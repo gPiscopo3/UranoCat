efd58d84c5275ec236a97b697499d485be9bd397
alicesalierno <alice.salierno@gmail.com>
2024-09-30 13:15:24 +0200

    main menu +ui

---
+++ Assets/Resources/MenuManager/ButtonPanelUI.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPanelUI : MonoBehaviour{
+
+    [SerializeField] public Button button;
+    [SerializeField] private TMP_Text lastAccess;
+    [SerializeField] private TMP_Text hourPlayed;
+
+    public void SetButtonText(string text){
+        this.button.GetComponentInChildren<TMP_Text>().text = text;
+        this.button.enabled = true;
+       
+    }
+
+    public void SetLastAccess(string text){
+        this.lastAccess.text = text;
+        this.lastAccess.enabled = true;
+    }
+    
+    public void SetHourPlayed(string text){
+        this.hourPlayed.text = text;
+        this.hourPlayed.enabled = true;
+    }
+
+    public void SetButtonInteractable(bool value){
+        this.button.interactable = value;
+    }
+    
+
+}
