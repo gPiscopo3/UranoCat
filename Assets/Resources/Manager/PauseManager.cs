1623eeeb1ab0d5faf510003754652667727e4b51
alicesalierno <alice.salierno@gmail.com>
2024-10-08 15:52:59 +0200

    Auto stash before merge of "cris_mission_branch" and "origin/cris_mission_branch"

--- Assets/Resources/Manager/PauseManager.cs
+++ Assets/Resources/Manager/PauseManager.cs
@@ -10,12 +10,17 @@ public class PauseManager : MonoBehaviour
     [SerializeField] private Button mainMenuButton;
     [SerializeField] private Button desktopButton;
     [SerializeField] private Button continueButton;
+    [SerializeField] private Button CommandsButton;
+    [SerializeField] private Button backButton;
 
     private GameLoader gameLoader;
 
     private PlayerController playerController;
 
     [SerializeField] protected GameObject pausePanel;
+    [SerializeField] protected GameObject commandsPanel;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +29,8 @@ public class PauseManager : MonoBehaviour
         continueButton.onClick.AddListener(Continue);
         mainMenuButton.onClick.AddListener(mainMenu);
         desktopButton.onClick.AddListener(desktop);
+        CommandsButton.onClick.AddListener(Commands);
+        backButton.onClick.AddListener(() => commandsPanel.SetActive(false));
 
         gameLoader = FindObjectOfType<GameLoader>();
     }
@@ -45,6 +52,11 @@ public class PauseManager : MonoBehaviour
         // da testare quando viene fatta la build
         gameLoader.SaveData("last");
         Application.Quit();
+    }
+
+    public void Commands()
+    {
+        commandsPanel.SetActive(true);
     }
 
 }
